18c13c2603ea35a539b24d2214da5d963b15cc64
Matthew Chick <c.chick@unsw.edu.au>
2019-09-11 20:24:30 +1000

    call http node module and create a simple server

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const total = helpers.sum(10,200);
 */
 
 const { add, sub } = require("./helper");  
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    res.end("hello world from node.js");
+})
+
+server.listen(3000);
 
 const total = add(10,200);
 console.log("Total: ", total);
