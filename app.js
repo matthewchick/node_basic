614870affc3c0e8ef779c4c85a07f684e3ad4143
Matthew Chick <c.chick@unsw.edu.au>
2019-09-11 21:26:22 +1000

    implement async and sync programming

--- app.js
+++ app.js
@@ -4,6 +4,37 @@ const total = helpers.sum(10,200);
 */
 
 const { add, sub } = require("./helper");  
+// test event loop
+const fs = require('fs')
+const filename = "target.txt"
+
+// sync programming
+const data = fs.readFileSync(filename);
+console.log(data.toString());
+
+/* Implement async programming
+// fs.watch(filename, () => console.log('File changed'));
+fs.readFile(filename, (err, data) => {
+    if (err){
+        console.log(err);
+    } 
+    console.log(data.toString());
+});
+*/
+console.log("Node js async programming (non blocking event loop)....")
+
+// Use express to perform a web server
+const express = require('express')
+
+const app = express()
+
+// use get method, (req, res) is callback function
+app.get('/', (req, res) => {
+    res.send("hey what up from express");
+})
+
+app.listen(3000);
+/*
 const http = require('http')
 
 const server = http.createServer((req, res) => {
@@ -11,6 +42,7 @@ const server = http.createServer((req, res) => {
 })
 
 server.listen(3000);
+*/
 
 const total = add(10,200);
 console.log("Total: ", total);
