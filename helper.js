4f62fd08515b49e3d0e683f3cb8f11f07dafe850
Matthew Chick <c.chick@unsw.edu.au>
2019-09-11 20:12:26 +1000

    use exports.sum instead of two statements

--- helper.js
+++ helper.js
@@ -3,5 +3,9 @@
 // }
 
 // use arrow function
-const sum = (a,b) => { return a + b }
+// const sum = (a,b) => { return a + b }
+/*
+const sum = (a, b) => a + b;
 module.exports = { sum };
+*/
+exports.sum = (a, b) => a + b;
