dfb2098f57ef36eb26db77129e22811c19009c84
Matthew Chick <c.chick@unsw.edu.au>
2019-09-11 20:39:24 +1000

    Use npm run dev to run nodemon

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const { add, sub } = require("./helper");
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    res.end("hello world from node.js");
+    res.end("hello world from node.js ");
 })
 
 server.listen(3000);
